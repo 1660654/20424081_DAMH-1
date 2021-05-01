8dac6a2f9ef389a9310059d0d5db0067d503ac1a
Lê Văn Trọng <levantrong@MacBook-Pro-cua-Le.local>
2021-05-01 21:19:52 +0700

    update code create Class SlangWord

--- src/pkg20424081_9/Main.java
+++ src/pkg20424081_9/Main.java
@@ -17,11 +17,16 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+            ArrayList<String> Definitions = new ArrayList<>();
+            Definitions.add("trong");
+            Definitions.add("tresr");
+            Definitions.add("tesrt");
+            System.out.println(Definitions.contains("tron"));
         // TODO code application logic here
-        SlangWordList slangwordlist = new SlangWordList();
-        System.out.println("Initial Mappings are: 1");
-        slangwordlist.ImportSlangWordList("slang.txt");
-//        slangwordlist.XuatListHocSinh();
-        slangwordlist.SearchValue("Initial");
+//        SlangWordList slangwordlist = new SlangWordList();
+//        System.out.println("Initial Mappings are: 1");
+//        slangwordlist.ImportSlangWordList("slang.txt");
+////        slangwordlist.XuatListHocSinh();
+//        slangwordlist.SearchValue("Initial");
     }
 }
--- src/pkg20424081_9/SlangWord.java
+++ src/pkg20424081_9/SlangWord.java
@@ -12,5 +12,40 @@ import java.util.*;
  */
 public class SlangWord {
     private String SlangWord;
-    ArrayList<String> Definitions = new ArrayList<>();
+    private String Definitions;
+    
+    public SlangWord(){
+        SlangWord = "";
+        Definitions = "";
+    }
+    
+    public SlangWord(String slangword, String definitions){
+        SlangWord = slangword;
+        Definitions = definitions;
+    }
+    
+    public boolean SearchDefinition(String searchText){
+        if(Definitions.contains(searchText)){
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean ChangeDefinition(String definitions){
+        Definitions = definitions;
+        return true;
+    }
+    
+    public boolean ChangeSlangWord(String slangword){
+        if(SlangWord.contains(slangword)){
+            return false;
+        }else{
+            SlangWord = slangword;
+        }
+        return true;
+    }
+    public boolean AddDefinition(String definitions){
+        Definitions += definitions;
+        return true;
+    }
 }
